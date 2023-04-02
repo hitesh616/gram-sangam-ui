9852f4fdb30d387c073ae43310597ab44f563eee
saurabh <saurabhshahane87@gmail.com>
2023-04-02 11:47:10 +0530

    initial commit

---
+++ app/src/main/java/com/example/gramsangam/Registartion.java
@@ -0,0 +1,14 @@
+package com.example.gramsangam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registartion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registartion);
+    }
+}
